351df78800489e661b430384e1641f1f17ff28c5
Lokolin <lokolin777@mail.ru>
2020-05-08 10:45:00 +0300

    fixes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import http from 'http';
-import express from 'express';
-import cors from 'cors';
-import io from 'socket.io';
-import config from '../config/config.json';
-import path from 'path';
+import http from "http";
+import express from "express";
+import cors from "cors";
+import io from "socket.io";
+import config from "../config/config.json";
+import path from "path";
 
 // setup server
 const app = express();
@@ -15,8 +15,8 @@ const socketIo = io(server);
 app.use(cors());
 
 // Render a API index page
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve('public/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("public/index.html"));
 });
 
 // Start listening
@@ -24,18 +24,18 @@ server.listen(process.env.PORT || config.port);
 console.log(`Started on port ${config.port}`);
 
 // Setup socket.io
-socketIo.on('connection', socket => {
+socketIo.on("connection", (socket) => {
   const username = socket.handshake.query.username;
   console.log(`${username} connected`);
 
-  socket.on('client:message', data => {
+  socket.on("client:message", (data) => {
     console.log(`${data.username}: ${data.message}`);
 
     // message received from client, now broadcast it to everyone else
-    socket.broadcast.emit('server:message', data);
+    socket.broadcast.emit("server:message", data);
   });
 
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
     console.log(`${username} disconnected`);
   });
 });
